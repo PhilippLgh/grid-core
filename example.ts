64294df408fe5566b11de522cc41a076580081f1
philipplgh <philipplgh@gmail.com>
2019-11-16 17:36:17 +0100

    re-org

---
+++ example.ts
@@ -0,0 +1,26 @@
+import Grid from './src/Grid'
+import { STATES } from './src/Clients/Client'
+
+const grid = new Grid()
+
+const startApi = async () => {
+  console.log('start grid server')
+  await grid.startApiServer()
+}
+
+const runGeth = async () => {
+  // creates a ClientManager instance from a plugin
+  const gethManager = await grid.getClientManager('geth')
+  // fetches release info, finds latest, downloads and extracts binary if necessary
+  const geth = await gethManager.getClient()
+  // creates a spawned aka (lifecycle-)ControlledProcess
+  await geth.start()
+  // awaits the `CONNECTED` state
+  await geth.stateChangedTo(STATES.CONNECTED)
+  // calls Geth's RPC server
+  const result = await geth.rpc('--version')
+  console.log(`result of --version: ${result}`)
+}
+
+startApi()
+// runGeth()
--- index.ts
+++ index.ts
@@ -1,24 +1,7 @@
-import Grid from './Grid'
-import { STATES } from './Clients/Client'
+import Grid from './src/Grid'
+import { start as startServer } from './src/ApiServer'
 
-const startApi = async () => {
-  console.log('start grid server')
-  await Grid.startServer()
-}
+export { startServer }
+export default Grid
 
-const runGeth = async () => {
-  // creates a ClientManager instance from a plugin
-  const gethManager = await Grid.getClientManager('geth')
-  // fetches release info, finds latest, downloads and extracts binary if necessary
-  const geth = await gethManager.getClient()
-  // creates a spawned aka (lifecycle-)ControlledProcess
-  await geth.start()
-  // awaits the `CONNECTED` state
-  await geth.stateChangedTo(STATES.CONNECTED)
-  // calls Geth's RPC server
-  const result = await geth.rpc('--version')
-  console.log(`result of --version: ${result}`)
-}
-
-// startApi()
-runGeth()
+// allow to run server from CLI
