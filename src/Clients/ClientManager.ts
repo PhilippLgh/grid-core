a2c32ae50697adeb55b3ad68759534ad77ec013d
PhilippLgh <philipp@ethereum.org>
2019-11-26 15:22:08 +0100

    fix: support for filters and package extraction

--- src/Clients/ClientManager.ts
+++ src/Clients/ClientManager.ts
@@ -2,16 +2,38 @@ import path from 'path'
 import fs from 'fs'
 import Client from './Client'
 import { PackageManager, IPackage } from 'ethpkg'
-import PluginManager from '../PluginSystem/PluginManager'
-import { uuid } from '../util'
+import { uuid, resolveRuntimeDependency } from '../util'
+import { IRelease } from 'ethpkg/dist/Fetcher/IRepository'
 
 export type ReleaseSpecifier = string
 
 export type StateListener = (newState: string, arg: any) => void
 
+type FilterFunction = (release: IRelease) => boolean
+
 interface FetchClientOptions {
   spec? : ReleaseSpecifier,
   listener?: StateListener 
+}
+
+export const CLIENT_FETCH_STATES = {
+  RESOLVE_VERSION_STARTED: 'resolve_package_start',
+  RESOLVE_VERSION_FINISHED: 'resolve_package_finished',
+  DOWNLOAD_STARTED: 'download_started',
+  DOWNLOAD_PROGRESS: 'download_progress',
+  DOWNLOAD_FINISHED: 'download_finished',
+  VERIFICATION_ERROR: 'verification_error',
+  VERIFICATION_FAILED: 'verification_failed',
+  PACKAGE_WRITTEN: 'package_written',
+  BINARY_EXTRACTED: 'binary_extracted',
+
+  PACKAGE_EXTRACTION_STARTED: 'PACKAGE_EXTRACTION_STARTED',
+  PACKAGE_EXTRACTION_PROGRESS: 'PACKAGE_EXTRACTION_PROGRESS',
+  PACKAGE_EXTRACTION_FINISHED: 'PACKAGE_EXTRACTION_FINISHED',
+
+  RESOLVE_DEPENDENCIES_STARTED: 'RESOLVE_DEPENDENCIES_STARTED',
+  RESOLVE_DEPENDENCIES_FINISHED: 'RESOLVE_DEPENDENCIES_FINISHED',
+
 }
 
 export class ClientManager {
@@ -39,15 +61,54 @@ export class ClientManager {
     return this._config.about && this._config.about.description
   }
 
+  get repository() {
+    return this._config.repository
+  }
+
+  get filter() {
+    return this._config.filter
+  }
+
+  get unpack() {
+    return this._config.unpack === true
+  }
+
+  private createFilter = (filterConfig: any) : FilterFunction => {
+    if (!filterConfig || !('name' in filterConfig)) {
+      return (() => true)
+    }
+    const { name } = filterConfig
+    const includes : Array<string> = name.includes || []
+    const excludes: Array<string> = name.excludes || []
+    return ({ fileName, version } : any) => {
+      if (!fileName) {
+        return false
+      }
+      fileName = fileName.toLowerCase()
+      return (
+        (!includes || includes.every(val => fileName.indexOf(val) >= 0)) &&
+        (!excludes || excludes.every(val => fileName.indexOf(val) === -1))
+      )
+    }
+  }
+
   async getVersions() {
-    return this._packageManager.listPackages('azure:gethstore')
+    const filter = this.filter ? this.createFilter(this.filter) : undefined
+    return this._packageManager.listPackages(this.repository, {
+      filter
+    })
+  }
+
+  async resolve(spec: string, listener: StateListener) {
+    const filter = this.filter ? this.createFilter(this.filter) : undefined
+    return this._packageManager.findPackage(spec, { listener, filter })
   }
 
   async getAllClients() {
     return <Array<Client>>[]
   }
 
-  private async extractBinary(pkg : IPackage) {
+  private async extractBinary(pkg : IPackage, destPath: string) {
     const entries = await pkg.getEntries()
     // const entries = await this.updater.getEntries(pkgPathOrUrl)
     if (entries.length === 0) {
@@ -79,7 +140,7 @@ export class ClientManager {
     }
 
     // FIXME use proper caching here
-    const destAbs = path.join(process.cwd(), 'temp', binaryName) //path.join(this.cacheDir, binaryName)
+    const destAbs = path.join(destPath, binaryName) //path.join(this.cacheDir, binaryName)
     // The unlinking might fail if the binary is e.g. being used by another instance
     if (fs.existsSync(destAbs)) {
       fs.unlinkSync(destAbs)
@@ -101,51 +162,65 @@ export class ClientManager {
     listener = (newState: string, arg: any) => undefined
   } : FetchClientOptions = {}) : Promise<Client> {
 
-    // FIXME use proper caching
-    let cachedBinaryPath = path.join(process.cwd(), 'temp', 'geth')
-    if (fs.existsSync(cachedBinaryPath)) {
-      return new Client(cachedBinaryPath, this._config)
+    const repo = this.repository
+    
+    const TEMP = path.join(process.cwd(), 'grid_temp')
+    if (!fs.existsSync(TEMP)) {
+      fs.mkdirSync(TEMP)
     }
 
-    // FIXME use spec to fetch correct client
     // FIXME get client package and extract binary here
+    const filter = this.filter ? this.createFilter(this.filter) : undefined
     const pkg : IPackage | undefined = await this._packageManager.getPackage({
-      spec: 'azure:gethstore',
-      listener: (newState, arg) => {
-        listener(newState, arg)
-        if ('progress' in arg) {
-          console.log(`download progress: ${arg.progress}%`)
-        } else {
-          console.log(newState)
-        }
-      }
+      spec: `${repo}`,
+      listener,
+      filter,
+      cache: TEMP
     })
     if (!pkg) {
       throw new Error('Could not fetch the package')
     }
-    console.log('start extracting binary')
-    const binaryPath = await this.extractBinary(pkg)
-    console.log('binary extracted', binaryPath)
+    if (pkg.metadata) {
+      // TODO fire listener
+      const { fileName } = pkg.metadata
+      let cachedBinaryPath = path.join(TEMP, fileName)
+      // reading and writing to same file can cause issues -> don't attempt overwrite of cached data
+      if (!fs.existsSync(cachedBinaryPath)) {
+        await pkg.writePackage(cachedBinaryPath)
+      }
+    }
 
-    return new Client(binaryPath, this._config)
+    if (this.unpack) {
+
+      // FIXME this is not always correct: "name" not always the the package content name
+      let name = pkg.metadata && pkg.metadata.name
+      let packagePath : string | undefined = name ? path.join(TEMP, name) : undefined
+      if (!packagePath || !fs.existsSync(packagePath)) {
+        packagePath = undefined
+        // client needs to be unpacked
+        listener(CLIENT_FETCH_STATES.PACKAGE_EXTRACTION_STARTED, {})
+        packagePath = await pkg.extract(TEMP, (progress : number, file: string) => {
+          listener(CLIENT_FETCH_STATES.PACKAGE_EXTRACTION_PROGRESS, {
+            progress,
+            file
+          })
+        })
+        listener(CLIENT_FETCH_STATES.PACKAGE_EXTRACTION_FINISHED, { packageContentsPath: packagePath })
+      }
+
+      listener(CLIENT_FETCH_STATES.RESOLVE_DEPENDENCIES_STARTED, {})
+      const JAVA_PATH = await resolveRuntimeDependency({
+        name: 'Java'
+      })
+      listener(CLIENT_FETCH_STATES.RESOLVE_DEPENDENCIES_FINISHED, {})
+
+      const binaryPath = JAVA_PATH
+      return new Client(binaryPath, this._config, packagePath)
+    } else {
+      console.log('start extracting binary')
+      const binaryPath = await this.extractBinary(pkg, TEMP)
+      console.log('binary extracted', binaryPath)
+      return new Client(binaryPath, this._config)
+    }
   }
-
 }
-
-/*
-Client Managers are stateful and have uuids they should not be created on the fly
-/**
- * Client Manager Factor:
- * creates a ClientManager instance based on a plugin / client name
- * @param name
- /
-export const getClientManager = async (name : string, pluginManager: PluginManager) => {
-  // TODO await pluginManager.whenReady()
-  const plugin = await pluginManager.getPlugin(name)
-  if (!plugin) {
-    throw new Error(`Plugin "${name}" not found`)
-  }
-  const { pluginExports: clientConfig } = plugin
-  return new ClientManager(clientConfig)
-}
-*/
