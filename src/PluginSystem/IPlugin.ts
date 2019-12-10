5760521569d25474bd2eddf204c92190762e8a05
PhilippLgh <philipp@ethereum.org>
2019-12-10 11:38:58 +0100

    feat: include resources in IPlugin

--- src/PluginSystem/IPlugin.ts
+++ src/PluginSystem/IPlugin.ts
@@ -1,4 +1,7 @@
+import { IPackage } from "ethpkg";
+
 export default interface IPlugin {
+  pkg?: IPackage; // if plugin is a package this must be set to the package instance
   readonly name: string; // unique identifier
   source : string; // the plugin source
   pluginExports : any; // the result of executing the plugin (module exports)
