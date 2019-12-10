08a80dfb5148440bdeb160db02d6736b7dec601e
PhilippLgh <philipp@ethereum.org>
2019-12-10 11:43:51 +0100

    feat: use proxy to inject plugin resources in API

--- src/PluginContext.ts
+++ src/PluginContext.ts
@@ -2,15 +2,34 @@ import { printFetchStateToCLI } from './Bin/cli-utils'
 
 // warning circular dependency
 import Grid, { GLOBAL_SETTINGS, WORKFLOW_TAGS, PLUGIN_TYPES } from './Grid'
+import { IPackage } from 'ethpkg'
 
 const oneOf = (arg: any) => { return arg}
 const multiSelect = (arg: any) => { return arg}
 
-
-export const createContext = (grid: Grid) => {
+export const createContext = (grid: Grid, insecure = false) => {
   // context needs to be a new one for each plugin
   // therefore we use a factory
-  return () => {
+  return (pluginResources : IPackage) => {
+
+    // intercept calls to grid instance (beware singleton!) and augment them with plugin context if necessary
+    let handler = {
+      get(target: any, methodName: any, receiver: any) {
+        const origMethod = target[methodName];
+        if (typeof origMethod === 'function') {
+          // @ts-ignore
+          return function (...args) {
+            if (methodName === 'createClientManager') {
+              args.push(pluginResources)
+            }
+            // @ts-ignore
+            return origMethod.apply(this, args)
+          };
+        }
+        return origMethod
+      }
+    };
+
     const sandbox = {
       process: {
         platform: process.platform,
@@ -22,9 +41,9 @@ export const createContext = (grid: Grid) => {
       printFetchStateToCLI,
       // TODO allow insecure mode where require is available
       require: (name: string) => {
-        console.log('require called', name)
+        // console.log('require called', name)
         if (name === 'grid-core') {
-          return grid
+          return new Proxy(grid, handler)
         }
         if (name === 'grid-core/constants/workflow_tags') {
           return WORKFLOW_TAGS
@@ -38,6 +57,9 @@ export const createContext = (grid: Grid) => {
             multiSelect,
             GLOBAL_SETTINGS
           }
+        }
+        if (insecure === true) {
+          return require(name)
         }
         throw new Error(`module ${name} cannot be required. see TODO`)
       },
