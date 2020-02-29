c47810d04df04a2f47c8c2f26d385c6e71997e4e
PhilippLgh <philipp@ethereum.org>
2020-03-01 00:00:43 +0100

    chore: export workflow utils

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Grid from './Grid'
 import { start as startServer } from './ApiServer'
 
+import * as WorkflowUtils from './Workflows/utils'
+export { WorkflowUtils }
+
 export { startServer }
 export default Grid
 
