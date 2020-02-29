dba575415ecab47727ccd4ac981be2c1bafae95c
PhilippLgh <philipp@ethereum.org>
2020-02-29 23:23:47 +0100

    chore: add package.json to plugin metadata

--- src/PluginSystem/IPlugin.ts
+++ src/PluginSystem/IPlugin.ts
@@ -4,5 +4,6 @@ export default interface IPlugin {
   pkg?: IPackage; // if plugin is a package this must be set to the package instance
   readonly name: string; // unique identifier
   source : string; // the plugin source
+  pkgJson: any; // the parsed package.json
   pluginExports : any; // the result of executing the plugin (module exports)
 }
