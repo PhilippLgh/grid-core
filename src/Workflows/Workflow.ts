bd927ea28f858f56a342d9f171d8fe9b480d7525
PhilippLgh <philipp@ethereum.org>
2020-02-29 23:31:21 +0100

    chore: refactor workflow

--- src/Workflows/Workflow.ts
+++ src/Workflows/Workflow.ts
@@ -1,33 +1,69 @@
 import ISerializable from "../ISerializable"
+import IPlugin from "../PluginSystem/IPlugin"
 
 export default class Workflow implements ISerializable {
-  plugin: any
+  pluginExports: any
+  meta: any
 
-  constructor(plugin : any) {
-    this.plugin = plugin
+  constructor(plugin : IPlugin) {
+    this.pluginExports = plugin.pluginExports
+    this.meta = plugin.pkgJson
   }
 
   get name() {
-    return this.plugin.name
+    return this.meta.name
   }
 
   get displayName() {
-    return this.plugin.displayName || '<unknown>'
+    return this.meta.displayName || '<unknown>'
+  }
+
+  get version() {
+    return this.meta.version
+  }
+
+  get description() {
+    return this.meta.description
   }
 
   get tags() {
-    return this.plugin.tags || []
+    return this.pluginExports.tags || []
   }
 
   get exports() {
-    return this.plugin.exports
+    return this.pluginExports.exports
+  }
+
+  private async exitWorkflow(error?: Error) {
+    if (this.pluginExports.onStop) {
+      try {
+        await this.pluginExports.onStop()
+      } catch (error) {
+        // TODO log problems in stop
+      }
+    }
+    if (error) {
+      console.error(`Error in workflow "${this.name}":`, error)
+      throw error
+    }
   }
 
   async run(config?: any) {
-    if (!this.plugin.run) {
+    if (!this.pluginExports.run) {
       //await _default.run(config)
     } else {
-      await this.plugin.run(config)
+      if (typeof this.pluginExports.run.fn === 'function') {
+        try {
+          return await this.pluginExports.run.fn(config)
+        } catch (error) {
+          await this.exitWorkflow(error)
+        }
+      }
+      try {
+        return await this.pluginExports.run(config)
+      } catch (error) {
+        await this.exitWorkflow(error)
+      }
     }
   }
 
