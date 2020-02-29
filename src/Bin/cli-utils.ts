16a6e4052fbac5f7885eb58b4534afaf1382903c
PhilippLgh <philipp@ethereum.org>
2020-02-29 23:33:20 +0100

    chore: add more event listeners to cli printer

--- src/Bin/cli-utils.ts
+++ src/Bin/cli-utils.ts
@@ -1,6 +1,7 @@
 import ora from 'ora'
 import chalk from 'chalk'
-import { INIT_CLIENT_EVENTS } from '../Grid'
+import { PROCESS_EVENTS } from '../Grid'
+import { IPackage } from 'ethpkg'
 
 class Printer {
   handlers: any = {}
@@ -8,6 +9,15 @@ class Printer {
   add(taskName: string, handler: (arg: any) => any) {
     this.handlers[taskName] = handler
     return this
+  }
+  cancelTasks() {
+    const keys = Object.keys(this.context)
+    for (const key of keys) {
+      const val = this.context[key]
+      if (val && typeof val.fail === 'function' && val.isSpinning) {
+        val.fail()
+      }
+    }
   }
   print(taskName: string, args: any) {
     if (this.handlers[taskName]) {
@@ -25,34 +35,37 @@ class Printer {
   }
 }
 
-export const printFetchStateToCLI = (clientName: string) => {
-    
-  let spinner : ora.Ora | undefined
-  const EVENTS = INIT_CLIENT_EVENTS
+export const cliPrint = (str: string) => {
+  ora(str).start().succeed()
+}
 
-  // FIXME move logic to fetcher
-  let platform = process.platform
-  let version = 'latest'
-
-  const _ = INIT_CLIENT_EVENTS
+const createPrinter = () => {
+  const _ =  PROCESS_EVENTS
 
   const printer = new Printer()
   .add(_.CLIENT_INIT_STARTED, ({ name }) => {
     ora(`Client ${name} initialization started`).start().succeed()
   })
-  .add(_.RESOLVE_RELEASE_STARTED, () => {
-    // FIXME const { platform, version} = args
-    const resolveReleaseTask = ora(`Finding specified release: ${clientName} - ${platform}/${version}`).start()
+  .add(_.CLIENT_START_STARTED, ({ name, flags }) => {
+    const clientStartTask = ora(`Starting client ${name} with flags: ${JSON.stringify(flags)}`).start()
+    return { clientStartTask }
+  })
+  .add(_.CLIENT_START_FINISHED, ({ clientStartTask }) => {
+    clientStartTask.succeed()
+    return { clientStartTask: undefined }
+  })
+  .add(_.RESOLVE_RELEASE_STARTED, ({platform, version, name}) => {
+    const resolveReleaseTask = ora(`Finding specified release: ${name} - ${platform}/${version}`).start()
     return { resolveReleaseTask }
   })
-  .add(_.RESOLVE_RELEASE_FINISHED, ({ resolveReleaseTask, release, platform, version }) => {
+  .add(_.RESOLVE_RELEASE_FINISHED, ({ resolveReleaseTask, release, platform, version, name }) => {
     // TODO pretty print release vs json
     // FIXME handle release not found with task.fail
     const releaseInfo = chalk.gray(JSON.stringify(release, null, 2))
-    resolveReleaseTask.succeed(`Found release for: ${clientName} - ${platform}/${version}\n${releaseInfo}`)
+    resolveReleaseTask.succeed(`Found release for: ${name} - ${platform}/${version}\n${releaseInfo}`)
   })
   .add(_.DOWNLOAD_STARTED, ({ location }) => {
-    console.log(`Downloading client binaries from ${chalk.cyan(location)}`)
+    cliPrint(`Downloading client binaries from: ${chalk.cyan(location)}`)
     const downloadTask = ora('Downloading binaries: 0%').start()
     return { downloadTask }
   })
@@ -87,12 +100,16 @@ export const printFetchStateToCLI = (clientName: string) => {
     extractionTask.succeed(`Package extracted: 100% - ${packageContentsPath}`)
     return { extractionTask: undefined }
   })
-  .add(_.BINARY_EXTRACTION_STARTED, ({  }) => {
-    const binExtractionTask = ora('Extracting binary').start()
+  .add(_.BINARY_EXTRACTION_STARTED, ({ packagePath }) => {
+    const binExtractionTask = ora('Extracting binary from package '+(packagePath || '')).start()
     return  { binExtractionTask }
   })
-  .add(_.BINARY_EXTRACTION_FINISHED, ({ binExtractionTask, binaryPath  }) => {
-    binExtractionTask.succeed(`Binary written to: ${binaryPath}`)
+  .add(_.BINARY_EXTRACTION_PROGRESS, ({ binExtractionTask, binaryPathPackage }) => {
+    binExtractionTask.text = `Extracting binary "${binaryPathPackage}" from package`
+    return  { binExtractionTask }
+  })
+  .add(_.BINARY_EXTRACTION_FINISHED, ({ binExtractionTask, binaryPathPackage, binaryPathFs  }) => {
+    binExtractionTask.succeed(`Binary "${binaryPathPackage}" written to: ${binaryPathFs}`)
     return  { binExtractionTask: undefined }
   })
   .add(_.CREATE_DOCKER_IMAGE_FROM_FILE_STARTED, () => {
@@ -116,9 +133,12 @@ export const printFetchStateToCLI = (clientName: string) => {
     const searchContainerTask = ora('Searching for existing Docker container').start()
     return { searchContainerTask }
   })
-  .add(_.FIND_EXISTING_DOCKER_CONTAINER_FINISHED, ({ searchContainerTask }) => {
-    // FIXME check if found
-    searchContainerTask.succeed('Found existing Docker container')
+  .add(_.FIND_EXISTING_DOCKER_CONTAINER_FINISHED, ({ searchContainerTask, name: imageName, container }) => {
+    if (container) {
+      searchContainerTask.succeed('Found existing Docker container')
+    } else {
+      searchContainerTask.fail(`No existing container found for "${imageName}"`)
+    }
     return { searchContainerTask: undefined }
   })
   .add(_.CREATE_DOCKER_CONTAINER_STARTED, () => {
@@ -133,11 +153,107 @@ export const printFetchStateToCLI = (clientName: string) => {
     ora('Docker client ready!').start().succeed()
   })
 
-  return (newState: string, args: any) => {
-    printer.print(newState, args)
-  }
+  .add(_.CREATE_PACKAGE_STARTED, () => {
+    const createPackageTask = ora('Creating workflow package').start()
+    return { createPackageTask }
+  })
+  .add(_.CREATE_PACKAGE_PROGRESS, ({ createPackageTask, file }) => {
+    createPackageTask.text = `Packing file: ${chalk.green(`${file}%`)}`
+  })
+  .add(_.CREATE_PACKAGE_FINISHED, ({ createPackageTask, name: packageName, pkg }) => {
+    createPackageTask.succeed(`Created workflow package ${ packageName }`)
+    prettyPrintPackage(pkg)    
+    return { createPackageTask: undefined }
+  })
+
+  .add(_.CREATE_SIGNING_KEY_STARTED, ({ alias }) => {
+    // don't use task here since user input might be requested
+    cliPrint('Creating signing key: '+alias)
+  })
+  .add(_.CREATE_SIGNING_KEY_FINISHED, ({ keyPath }) => {
+    cliPrint(`Signing key written to: ${ chalk.bold(keyPath) }`)
+  })
+
+  .add(_.UNLOCKING_KEY_STARTED, () => {
+    const unlockKeyTask = ora('Unlocking signing key').start()
+    return { unlockKeyTask }
+  })
+  .add(_.UNLOCKING_KEY_FINISHED, ({ unlockKeyTask, address }) => {
+    unlockKeyTask.succeed(`Unlocked signing key ${address}`)
+  })
+
+  .add(_.SIGN_WORKFLOW_PKG_STARTED, () => {
+    const signPkgTask = ora('Signing workflow package').start()
+    return { signPkgTask }
+  })
+  .add(_.SIGN_WORKFLOW_PKG_FINISHED, ({ signPkgTask }) => {
+    signPkgTask.succeed('Workflow package signed')
+  })
+
+  .add(_.REPOSITORY_LOGIN_STARTED, () => {
+    const loginTask = ora('Authenticating with repository').start()
+    return { loginTask }
+  })
+  .add(_.REPOSITORY_LOGIN_FINISHED, ({ loginTask, isLoggedIn }) => {
+    loginTask.succeed('Authentication finished. Logged in? '+isLoggedIn)
+  })
+
+  .add(_.UPLOAD_STARTED, ({ name: workflowName, repo }) => {
+    cliPrint(`Uploading workflow "${workflowName}" to: ${chalk.cyan(repo)}`)
+    const uploadTask = ora('Uploading workflow: 0%').start()
+    return { uploadTask }
+  })
+  .add(_.UPLOAD_PROGRESS, ({ uploadTask, progress }) => {
+    uploadTask.text = `Uploading workflow: ${chalk.green(`${progress}%`)}`
+  })
+  .add(_.UPLOAD_FINISHED, ({ uploadTask, name: workflowName }) => {
+      const releaseInfo = '' /*boxen(chalk.gray(JSON.stringify(verifiedRelease, null, 2)), {
+        padding: 1
+      })*/
+      uploadTask.succeed(`Uploaded workflow "${workflowName}": 100%`)
+  })
+
+  .add(_.APP_SERVER_START_STARTED, ({ app }) => {
+    const startServerTask = ora(`Starting app server for "${app ? app.fileName : '<unknown app>'}"`).start()
+    return {
+      startServerTask
+    }
+  })
+  .add(_.APP_SERVER_START_FINISHED, ({ startServerTask, app, url: appUrl }) => {
+    startServerTask.succeed(`App server for "${app ? app.name : '<unknown app>'}" running on: ${appUrl}`)
+    return {
+      startServerTask: undefined
+    }
+  })
+
+  return printer
+}
+
+export const prettyPrintPackage = async (pkg : IPackage) => {
+  const entries = await pkg.getEntries()
+
 }
 
 export const replaceAll = (str: string, searchVal: string, replaceVal: string ) : string => {
   return str.split(searchVal).join(replaceVal)
+}
+
+export const createCLIPrinter = () => {
+  const printer = createPrinter()
+  const listener = (newState: string, args: any) => {
+    // console.log('new state', newState, args)
+    printer.print(newState, args)
+  }
+  return {
+    listener,
+    print(message?: any, ...optionalParams: any[]) {
+      console.log(chalk.grey('Info: '+message), ...optionalParams)
+    },
+    fail(err: string | Error){
+      // TODO cancel all failed tasks
+      printer.cancelTasks()
+      const msg = typeof err === 'string' ? err : (err.message ? err.message : 'Unknown Error')
+      console.log(chalk.redBright(`Èrror: ${msg}`))
+    }
+  }
 }
