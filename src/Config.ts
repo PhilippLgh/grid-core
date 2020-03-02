0102d84d78e036adc0410fa212d98204bc875236
PhilippLgh <philipp@ethereum.org>
2020-03-02 15:06:37 +0100

    feat: add registry config

---
+++ src/Config.ts
@@ -0,0 +1,6 @@
+export const RegistryConfig = {
+  NAME: 'ethpkg',
+  OWNER: '0x59054E7c69D42FCcA9CC2f7Fd0BE52db00152669/workflows',
+  OWNER_QUERY_FRIENDLY: '0x59054E7c69D42FCcA9CC2f7Fd0BE52db00152669-workflows',
+  ALIAS: 'grid/workflows'
+}
